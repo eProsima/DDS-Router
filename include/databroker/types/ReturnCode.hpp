e6986daeb8a241644e4983431582edad9b8efaf8
jparisu <javierparis@eprosima.com>
2021-10-18 15:51:37 +0200

    Add main headers
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ include/databroker/types/ReturnCode.hpp
@@ -0,0 +1,38 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file ReturnCode.hpp
+ */
+
+#ifndef _DATABROKER_TYPES_TYPES_HPP_
+#define _DATABROKER_TYPES_TYPES_HPP_
+
+#include <fastrtps/types/TypesBase.h>
+
+namespace eprosima {
+namespace databroker {
+
+class ReturnCode: public eprosima::fastrtps::types::ReturnCode_t
+{
+    bool operator bool() const
+    {
+        return value_ == 0;
+    }
+}
+
+} /* namespace rtps */
+} /* namespace databroker */
+
+#endif /* _DATABROKER_TYPES_TYPES_HPP_ */
