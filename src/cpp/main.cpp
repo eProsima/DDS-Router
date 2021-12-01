1a0ef1e314bdabfcaa7e1ae5007447c3cc00c5af
jparisu <javierparis@eprosima.com>
2021-12-01 13:58:18 +0100

    Eliminate main so it is added in further PRs
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -17,82 +17,42 @@
  *
  */
 
-#include <ddsrouter/types/RawConfiguration.hpp>
-#include <ddsrouter/types/ReturnCode.hpp>
-#include <ddsrouter/types/Time.hpp>
-#include <ddsrouter/types/constants.hpp>
 #include <ddsrouter/core/DDSRouter.hpp>
-#include <ddsrouter/user_interface/FileWatcherHandler.hpp>
-#include <ddsrouter/user_interface/SignalHandler.hpp>
+#include <ddsrouter/participant/implementations/auxiliar/DummyParticipant.hpp>
+#include <ddsrouter/types/configuration_tags.hpp>
+#include <ddsrouter/types/utils.hpp>
+#include <ddsrouter/types/Log.hpp>
+#include <ddsrouter/types/constants.hpp>
 
 using namespace eprosima::ddsrouter;
-
 
 int main(
         int argc,
         char** argv)
 {
-    std::cout << "Starting DDS Router execution." << std::endl;
+    // TODO: main
 
-    // TODO do it depending on arguments
+    static_cast<void>(argc);
+    static_cast<void>(argv);
 
     // Activate log
     Log::SetVerbosity(Log::Kind::Info);
     Log::SetCategoryFilter(std::regex("(DDSROUTER)"));
 
-    // Encapsulating execution in block to erase all memory correctly before closing process
-    {
-        // Configuration File path
-        std::string file_path = DEFAULT_CONFIGURATION_FILE_NAME;
+    // Generate configuration
+    std::string file_name = DEFAULT_CONFIGURATION_FILE_NAME;
+    RawConfiguration configuration = load_configuration_from_file(file_name);
 
-        // Reload time
-        bool is_reload_set = false;
-        eprosima::ddsrouter::Duration_ms reload_time;
+    // Create DDSRouter entity
+    DDSRouter router(configuration);
 
-        // TODO parse arguments
+    // Start DDSRouter
+    router.start();
 
+    std::this_thread::sleep_for(std::chrono::seconds(60));
 
-        // Load DDS Router Configuration
-        RawConfiguration router_configuration = load_configuration_from_file(file_path);
+    // Stop DDS Router
+    router.stop();
 
-        // Create DDS Router
-        DDSRouter router(router_configuration);
-
-        // File Watcher Handler
-        // Callback will reload configuration and pass it to DDSRouter
-        std::function<void(std::string)> filewatcher_callback =
-            [&router]
-            (std::string file_path)
-            {
-                std::cout << "Reloading configuration." << std::endl;
-                try
-                {
-                    RawConfiguration router_configuration = load_configuration_from_file(file_path);
-                    router.reload_configuration(router_configuration);
-                }
-                catch(const std::exception& e)
-                {
-                    std::cerr <<
-                        "Error reloading configuration file " << file_path << " with error: " << e.what() <<
-                        std::endl;
-                }
-            };
-
-        // Creating FileWatcher event handler
-        interface::FileWatcherHandler file_watcher_handler(filewatcher_callback, file_path, reload_time);
-
-        // Signal handler
-        // std::unique_ptr<interface::SignalHandler<interface::Signals::SIGNAL_SIGINT>> signal_handler();
-        interface::SignalHandler<interface::SIGNAL_SIGINT> signal_handler;
-
-        // Wait until signal arrives
-        signal_handler.wait_for_event();
-
-        // Stopping DDS Router
-        router.stop();
-    }
-
-    std::cout << "Finishing DDS Router execution correctly." << std::endl;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
