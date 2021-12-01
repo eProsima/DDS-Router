4f60045378a336757e2a1afb8092d91a815e9ab2
jparisu <69341543+jparisu@users.noreply.github.com>
2021-12-01 12:20:52 +0100

    Address Implementation [13026] (#62)
    * Refs #13008: Implement Address classes
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13008: Add documentation
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13008: Apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13008: Add documentation to DiscoveryServerConnectionAddress
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13008: uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13008: fix minor typos
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ include/ddsrouter/types/address/DiscoveryServerConnectionAddress.hpp
@@ -0,0 +1,83 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// limitations under the License.
+
+/**
+ * @file DiscoveryServerConnectionAddress.hpp
+ *
+ */
+
+#ifndef _DDSROUTER_TYPES_ADDRESS_DISCOVERYSERVERCONNECTIONADDRESS_HPP_
+#define _DDSROUTER_TYPES_ADDRESS_DISCOVERYSERVERCONNECTIONADDRESS_HPP_
+
+#include <string>
+#include <vector>
+
+#include <ddsrouter/types/address/Address.hpp>
+#include <ddsrouter/types/endpoint/Guid.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+
+/**
+ * @brief Collection of Addresses to connect with a remote Disovery Server.
+ *
+ * An address will remain in an IP and a Port, IP version and Transport Protocol.
+ * This class has several address associated with one \c GuidPrefix in order to connect with
+ * a remote Discovery Server.
+ */class DiscoveryServerConnectionAddress
+{
+public:
+
+    /**
+     * @brief Construct a new \c DiscoveryServerConnectionAddress object with all the parameters
+     *
+     * @param discovery_server_guid_ : Guid Prefix of the remote Discovery Server
+     * @param addresses_ collection of addresses
+     */
+    DiscoveryServerConnectionAddress(
+            GuidPrefix discovery_server_guid,
+            std::set<Address> addresses);
+
+    //! Discovery Server \c GuidPrefix Port getter
+    GuidPrefix discovery_server_guid() const noexcept;
+
+    //! Addresses getter
+    std::set<Address> addresses() const noexcept;
+
+    /**
+     * @brief Whether the address is correct
+     *
+     * Checks if GuidPrefix is correct.
+     * Checks if it has at least one correct address.
+     */
+    virtual bool is_valid() const noexcept;
+
+protected:
+
+    //! Internal Discovery Server Guid Prefix object
+    GuidPrefix discovery_server_guid_;
+
+    //! Internal Addresses object
+    std::set<Address> addresses_;
+};
+
+//! \c DiscoveryServerConnectionAddress to stream serializator
+std::ostream& operator <<(
+        std::ostream& output,
+        const DiscoveryServerConnectionAddress& address);
+
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+#endif /* _DDSROUTER_TYPES_ADDRESS_DISCOVERYSERVERCONNECTIONADDRESS_HPP_ */
