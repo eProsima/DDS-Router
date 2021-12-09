2fff7518709ad1d351f5975de7ae2dd53b15b6d1
jparisu <javierparis@eprosima.com>
2021-12-09 14:44:52 +0100

    Refs #13008: Fix Payload to stream
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/types/Data.cpp
+++ src/cpp/types/Data.cpp
@@ -38,7 +38,7 @@ std::ostream& operator <<(
 {
     os << "Payload{";
 
-    for (int i = 0; i < (payload.length - 1); ++i)
+    for (int i = 0; (payload.length != 0) && (i < (payload.length - 1)); ++i)
     {
         os << payload.data[i] << " ";
     }
