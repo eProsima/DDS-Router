c542f21cac8a0f6f51e8a0701dbce7a56ac9f120
jparisu <javierparis@eprosima.com>
2021-12-20 10:49:31 +0100

    add windows support for test
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- test/system/executable/launcher.ps1
+++ test/system/executable/launcher.ps1
@@ -14,7 +14,7 @@ Param(
     [Parameter(Position=2, Mandatory=$true)]
     [ValidateScript({Test-Path $_ -PathType Leaf -IsValid })]
     [String]
-    # fast server creation binary full qualified path
+    # ddsrouter creation binary full qualified path
     $tool_path,
 
     [Parameter(Position=3, Mandatory=$true)]
@@ -26,7 +26,10 @@ Param(
 
 $test = Start-Process -Passthru -Wait `
     -FilePath $python_path `
-    -ArgumentList ($test_script, $tool_path, $test_name) `
+    -ArgumentList (
+        $test_script,
+        "--exe", $tool_path,
+        "--config-file", $test_name) `
     -WindowStyle Hidden
 
 if( $test.ExitCode -ne 0 )
