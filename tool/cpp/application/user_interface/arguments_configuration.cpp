7582f1a832a12dc1ae0e5a635a237e4b11f52fb1
jparisu <javierparis@eprosima.com>
2022-01-21 10:53:58 +0100

    Refs #13546: Refactor CMakes so it is a library
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- tool/cpp/application/user_interface/arguments_configuration.cpp
+++ tool/cpp/application/user_interface/arguments_configuration.cpp
@@ -30,7 +30,7 @@
 #endif // if defined(_WIN32)
 
 #include <ddsrouter/types/Log.hpp>
-#include <ddsrouter/user_interface/arguments_configuration.hpp>
+#include "arguments_configuration.hpp"
 
 namespace eprosima {
 namespace ddsrouter {
@@ -142,7 +142,7 @@ ProcessReturnCode parse_arguments(
         // Unknown args provided
         if (parse.nonOptionsCount())
         {
-            std::cerr << "ERROR: Unknown argument " << parse.nonOption(0) << std::endl;
+            std::cerr << "ERROR: Unknown argument: " << parse.nonOption(0) << std::endl;
             option::printUsage(fwrite, stdout, usage, columns);
             return ProcessReturnCode::INCORRECT_ARGUMENT;
         }
