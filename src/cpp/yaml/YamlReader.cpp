d731454aa3a6ba59d80bd42e554d1297d8f59322
jparisu <69341543+jparisu@users.noreply.github.com>
2022-02-01 11:50:42 +0100

    Yaml module [13576] (#129)
    * Refs #13576: Add yaml module
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: Add version template to get yaml methods
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: enable tool
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: Add yaml addres support
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: enable Trivial test
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: correct test compilation
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: Add error msg to yaml methods with tag
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: undo uncrustify in autogenerated files
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #13576: uncrustify ipp
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ src/cpp/yaml/YamlReader.cpp
@@ -0,0 +1,63 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file YamlReader.cpp
+ *
+ */
+
+#include <ddsrouter/types/address/Address.hpp>
+#include <ddsrouter/types/address/DiscoveryServerConnectionAddress.hpp>
+#include <ddsrouter/types/endpoint/DomainId.hpp>
+#include <ddsrouter/types/endpoint/GuidPrefix.hpp>
+#include <ddsrouter/types/topic/RealTopic.hpp>
+#include <ddsrouter/types/topic/WildcardTopic.hpp>
+#include <ddsrouter/yaml/YamlReader.hpp>
+#include <ddsrouter/yaml/yaml_configuration_tags.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace yaml {
+
+bool YamlReader::is_tag_present(
+        const Yaml& yml,
+        const TagType& tag)
+{
+    if (!yml.IsMap())
+    {
+        throw ConfigurationException(
+                  utils::Formatter() << "Trying to find a tag: <" << tag << "> in a not yaml object map.");
+    }
+
+    return (yml[tag]);
+}
+
+Yaml YamlReader::get_value_in_tag(
+        const Yaml& yml,
+        const TagType& tag)
+{
+    if (is_tag_present(yml, tag))
+    {
+        return yml[tag];
+    }
+    else
+    {
+        throw ConfigurationException(
+                  utils::Formatter() << "Required tag not found: <" << tag << ">.");
+    }
+}
+
+} /* namespace yaml */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
---
+++ test/unittest/yaml/YamlConfigurationTestUtils.hpp
@@ -0,0 +1,65 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file YamlConfigurationTestUtils.hpp
+ */
+
+#ifndef _DDSROUTER_TEST_UNITTEST_YAML_YAMLCONFIGURATIONTESTUTILS_HPP_
+#define _DDSROUTER_TEST_UNITTEST_YAML_YAMLCONFIGURATIONTESTUTILS_HPP_
+
+#include <ddsrouter/yaml/Yaml.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace yaml {
+namespace test {
+
+template <typename T>
+struct YamlField
+{
+    YamlField()
+        : present(false)
+    {
+    }
+
+    YamlField(
+            T arg_value)
+        : value(arg_value)
+        , present(true)
+    {
+    }
+
+    bool present;
+    T value;
+};
+
+template <typename T>
+void add_field_to_yaml(
+        Yaml& yml,
+        const YamlField<T>& field,
+        const std::string& tag)
+{
+    if (field.present)
+    {
+        yml[tag] = field.value;
+    }
+}
+
+} /* namespace test */
+} /* namespace yaml */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+#endif /* _DDSROUTER_TEST_UNITTEST_YAML_YAMLCONFIGURATIONTESTUTILS_HPP_ */
