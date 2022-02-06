e7c4e8727f95b35fcdc2ce8a345825acf28bdd5a
jparisu <javierparis@eprosima.com>
2022-02-06 11:38:48 +0100

    add test for discovery tool
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- test/tool/discovery/tests.py
+++ test/tool/discovery/tests.py
@@ -20,13 +20,11 @@ Usage: test.py -e <binary_path> -c <config_file_path>
 
 Arguments:
 
-    DDS Router binary path          : -e | --exe binary_path
+    DDS Router binary path : -e | --exe binary_path
 
-    DDS Router binary path          : -c | --config-file <config_file_path>
+    DDS Router binary path : -c | --config-file config_file_path
 
-    Run test in Debug mode          : -d | --debug
-
-    Use SIGINT or SIGTERM           : -s | --signal sigint|sigterm
+    Run test in Debug mode : -d | --debug
 """
 
 import argparse
@@ -36,11 +34,10 @@ import signal
 import subprocess
 import sys
 import time
-from enum import Enum
 
 DESCRIPTION = """Script to execute DDS Router executable test"""
 USAGE = ('python3 tests.py -e <path/to/ddsrouter-executable>'
-         ' -c config_file_path --signal sigint [-d]')
+         ' -c config_file_path [-d]')
 
 # Sleep time to let process init and finish
 SLEEP_TIME = 1
@@ -73,34 +70,6 @@ def file_exist_and_have_permissions(file_path):
         return None
 
 
-def is_linux():
-    """Return whether the script is running in a Linux environment."""
-    return os.name == 'posix'
-
-
-def is_windows():
-    """Return whether the script is running in a Windows environment."""
-    return os.name == 'nt'
-
-
-class KillingSignalType(Enum):
-    """Enumeration for signals used to kill subprocesses."""
-
-    KST_SIGINT = 2
-    KST_SIGTERM = 15
-
-
-def check_terminate_signal(st):
-    """Return signal that must be used to kill process otherwise."""
-    if st == 'sigterm':
-        return KillingSignalType.KST_SIGTERM
-    elif st == 'sigint':
-        return KillingSignalType.KST_SIGINT
-    else:
-        raise argparse.ArgumentTypeError(
-            f'Invalid value: {st}. It must be <sigint> or <sigterm>')
-
-
 def parse_options():
     """
     Parse arguments.
@@ -119,7 +88,7 @@ def parse_options():
         '--exe',
         type=file_exist_and_have_permissions,
         required=True,
-        help='Path to ddsrouter executable.'
+        help='Path to discovery executable.'
     )
     required_args.add_argument(
         '-c',
@@ -134,17 +103,10 @@ def parse_options():
         action='store_true',
         help='Print test debugging info.'
     )
-    parser.add_argument(
-        '-s',
-        '--signal',
-        type=check_terminate_signal,
-        required=True,
-        help='<sigint>|<sigterm>: Use SIGINT or SIGTERM to kill process.'
-    )
     return parser.parse_args()
 
 
-def test_ddsrouter_closure(ddsrouter, configuration_file, killing_signal):
+def test_ddsrouter_closure(ddsrouter, configuration_file):
     """
     Test that ddsrouter command closes correctly.
 
@@ -157,7 +119,6 @@ def test_ddsrouter_closure(ddsrouter, configuration_file, killing_signal):
     Parameters:
     ddsrouter (path): Path to ddsrouter binary executable
     configuration_file (path): Path to ddsrouter yaml configuration file
-    use_sigint (KillingSignalType): Signal to kill subprocesses
 
     Returns:
     0 if okay, otherwise the return code of the command executed
@@ -190,25 +151,17 @@ def test_ddsrouter_closure(ddsrouter, configuration_file, killing_signal):
         return 1
 
     # direct this script to ignore SIGINT in case of windows
-    if is_windows():
+    if os.name == 'nt':
         signal.signal(signal.SIGINT, signal_handler)
 
-    # send signal to process and wait for it to be killed
+    # send SIGINT to process and wait for processing
     lease = 0
     while True:
 
-        if killing_signal == KillingSignalType.KST_SIGTERM:
-            # Use SIGTERM instead
-            if is_linux():
-                proc.send_signal(signal.SIGTERM)
-            elif is_windows():
-                proc.send_signal(signal.CTRL_BREAK_EVENT)
-        else:
-            # Use SIGINT (use by default if not SIGTERM)
-            if is_linux():
-                proc.send_signal(signal.SIGINT)
-            elif is_windows():
-                proc.send_signal(signal.CTRL_C_EVENT)
+        if os.name == 'posix':
+            proc.send_signal(signal.SIGINT)
+        elif os.name == 'nt':
+            proc.send_signal(signal.CTRL_C_EVENT)
 
         time.sleep(SLEEP_TIME)
         lease += 1
@@ -276,8 +229,4 @@ if __name__ == '__main__':
             'executable permissions.')
         sys.exit(1)
 
-    sys.exit(
-        test_ddsrouter_closure(
-            args.exe,           # Path to executable
-            args.config_file,   # Configuration file
-            args.signal))       # Signal to kill subprocess
+    sys.exit(test_ddsrouter_closure(args.exe, args.config_file))
