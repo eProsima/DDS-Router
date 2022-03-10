8a6d71ff1c8c06261813347aff55233468795808
jparisu <69341543+jparisu@users.noreply.github.com>
2022-03-10 11:37:22 +0100

    Activate communication test (#158)
    * Refs #14020: Refactor communication tests to use configuration instead of yaml
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14020: apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14020: apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- ddsrouter_core/test/blackbox/ddsrouter_core/dds/WAN/DDSTestWAN.cpp
+++ ddsrouter_core/test/blackbox/ddsrouter_core/dds/WAN/DDSTestWAN.cpp
@@ -20,17 +20,169 @@
 #include <gtest/gtest.h>
 #include <TestLogHandler.hpp>
 
+#include <ddsrouter_core/configuration/participant/DiscoveryServerParticipantConfiguration.hpp>
 #include <ddsrouter_core/core/DDSRouter.hpp>
-#include <ddsrouter_utils/Log.hpp>
-#include <ddsrouter_core/types/RawConfiguration.hpp>
+#include <ddsrouter_core/types/address/Address.hpp>
+#include <ddsrouter_core/types/security/tls/TlsConfigurationBoth.hpp>
+#include <ddsrouter_core/types/security/tls/TlsConfigurationServer.hpp>
 
 #include <test_participants.hpp>
 
-using namespace eprosima::ddsrouter::core;
-using namespace eprosima::ddsrouter::core::types;
+namespace eprosima {
+namespace ddsrouter {
+namespace core {
+namespace test {
 
-constexpr const uint32_t SAMPLES_TO_RECEIVE = 5;
-constexpr const uint32_t MILLISECONDS_PUBLISH_LOOP = 100;
+enum WanKind
+{
+    SERVER,
+    CLIENT,
+    SERVER_AND_CLIENT
+};
+
+bool is_client(
+        WanKind wan_kind)
+{
+    return wan_kind == CLIENT || wan_kind == SERVER_AND_CLIENT;
+}
+
+bool is_server(
+        WanKind wan_kind)
+{
+    return wan_kind == SERVER || wan_kind == SERVER_AND_CLIENT;
+}
+
+constexpr const uint32_t DEFAULT_SAMPLES_TO_RECEIVE = 5;
+constexpr const uint32_t DEFAULT_MILLISECONDS_PUBLISH_LOOP = 100;
+constexpr const uint32_t DEFAULT_MESSAGE_SIZE = 1; // x50 bytes
+
+std::shared_ptr<types::security::TlsConfiguration> tls_configuration(
+        WanKind wan_kind)
+{
+    // TODO: test that using server with only Server required files works
+    // It fails when connecting to other server
+    if (is_server(wan_kind))
+    {
+        return std::make_shared<types::security::TlsConfigurationBoth>(
+            "../../resources/tls/ca.crt", // ca
+            "", // private key password
+            "../../resources/tls/ddsrouter.key", // private key
+            "../../resources/tls/ddsrouter.crt", // cert
+            "../../resources/tls/dh_params.pem" // dh params
+            );
+    }
+    else
+    {
+        return std::make_shared<types::security::TlsConfigurationClient>(
+            "../../resources/tls/ca.crt" // ca
+            );
+    }
+}
+
+std::shared_ptr<configuration::ParticipantConfiguration> wan_participant_configuration(
+        bool this_server_id_is_1,
+        WanKind wan_kind,
+        types::TransportProtocol transport_protocol,
+        types::IpVersion ip_version,
+        bool tls = false)
+{
+    std::set<types::Address> listening_addresses;
+    std::set<types::DiscoveryServerConnectionAddress> connection_addresses;
+
+    if (is_client(wan_kind))
+    {
+        connection_addresses.insert(
+            types::DiscoveryServerConnectionAddress(
+                types::GuidPrefix((this_server_id_is_1 ? 0u : 1u)),
+                        {
+                            types::Address(
+                                (ip_version == types::IpVersion::IPv4 ? "127.0.0.1" : "::1"),
+                                11666 + (this_server_id_is_1 ? 0u : 1u),
+                                ip_version,
+                                transport_protocol)
+                        }
+                )
+            );
+    }
+
+    if (is_server(wan_kind))
+    {
+        listening_addresses.insert(
+            types::Address(
+                (ip_version == types::IpVersion::IPv4 ? "127.0.0.1" : "::1"),
+                11666 + (this_server_id_is_1 ? 1u : 0u),
+                ip_version,
+                transport_protocol)
+            );
+    }
+
+    if (tls)
+    {
+        return std::make_shared<configuration::DiscoveryServerParticipantConfiguration>(
+            types::ParticipantId("WanParticipant_" + std::to_string((this_server_id_is_1 ? 1 : 0))),
+            types::GuidPrefix((this_server_id_is_1 ? 1u : 0u)),
+            listening_addresses,
+            connection_addresses,
+            types::ParticipantKind(types::ParticipantKind::WAN),
+            tls_configuration(wan_kind));
+
+    }
+    else
+    {
+        return std::make_shared<configuration::DiscoveryServerParticipantConfiguration>(
+            types::ParticipantId("WanParticipant_" + std::to_string((this_server_id_is_1 ? 1 : 0))),
+            types::GuidPrefix((this_server_id_is_1 ? 1u : 0u)),
+            listening_addresses,
+            connection_addresses,
+            types::ParticipantKind(types::ParticipantKind::WAN)
+            );
+    }
+}
+
+/**
+ * @brief Create a simple configuration for a DDS Router
+ *
+ * Create a configuration with 1 topic
+ * Create 1 simple participants with domains \c domain
+ * Create 1 custom participant by the configuration in \c participant_configuration
+ *
+ * @return configuration::DDSRouterConfiguration
+ */
+configuration::DDSRouterConfiguration router_configuration(
+        std::shared_ptr<configuration::ParticipantConfiguration> participant_configuration,
+        types::DomainIdType domain)
+{
+    std::set<std::shared_ptr<types::FilterTopic>> allowlist;   // empty
+    std::set<std::shared_ptr<types::FilterTopic>> blocklist;   // empty
+
+    // One topic
+    std::set<std::shared_ptr<types::RealTopic>> builtin_topics(
+                    {
+                        std::make_shared<types::RealTopic>("HelloWorldTopic", "HelloWorld"),
+                    });
+
+    // Two participants, one custom and other simple. If server, simple will work in 0, if not in 1
+    std::set<std::shared_ptr<configuration::ParticipantConfiguration>> participants_configurations(
+                    {
+                        // custom
+                        participant_configuration,
+
+                        // simple
+                        std::make_shared<configuration::SimpleParticipantConfiguration>(
+                            types::ParticipantId("simple_participant"),
+                            types::ParticipantKind(types::ParticipantKind::SIMPLE_RTPS),
+                            types::DomainId(domain)
+                            ),
+                    }
+        );
+
+    return configuration::DDSRouterConfiguration(
+        allowlist,
+        blocklist,
+        builtin_topics,
+        participants_configurations
+        );
+}
 
 /**
  * Test communication between two DDS Participants hosted in the same device, but which are at different DDS domains.
@@ -38,8 +190,11 @@ constexpr const uint32_t MILLISECONDS_PUBLISH_LOOP = 100;
  * instances communicate with the DDS Participants through Simple Participants deployed at those domains.
  */
 void test_WAN_communication(
-        std::string server_config_path,
-        std::string client_config_path)
+        configuration::DDSRouterConfiguration ddsrouter_server_configuration,
+        configuration::DDSRouterConfiguration ddsrouter_client_configuration,
+        uint32_t samples_to_receive = DEFAULT_SAMPLES_TO_RECEIVE,
+        uint32_t time_between_samples = DEFAULT_MILLISECONDS_PUBLISH_LOOP,
+        uint32_t msg_size = DEFAULT_MESSAGE_SIZE)
 {
     // Check there are no warnings/errors
     // TODO: Uncomment when having no listening addresses is no longer considered an error by the middleware
@@ -49,41 +204,44 @@ void test_WAN_communication(
     uint32_t samples_sent = 0;
     std::atomic<uint32_t> samples_received(0);
 
+    // Create a message with size specified by repeating the same string
     HelloWorld msg;
-    msg.message("Testing DDS-Router Blackbox WAN...");
+    std::string msg_str;
+
+    // Add this string as many times as the msg size requires
+    for (uint32_t i = 0; i < msg_size; i++)
+    {
+        msg_str += "Testing DDSRouter Blackbox Local Communication ...";
+    }
+    msg.message(msg_str);
 
     // Create DDS Publisher in domain 0
-    HelloWorldPublisher<HelloWorld> publisher;
+    TestPublisher<HelloWorld> publisher;
     ASSERT_TRUE(publisher.init(0));
 
     // Create DDS Subscriber in domain 1
-    HelloWorldSubscriber<HelloWorld> subscriber;
+    TestSubscriber<HelloWorld> subscriber;
     ASSERT_TRUE(subscriber.init(1, &msg, &samples_received));
 
-    // Load configuration containing a Simple Participant in domain 0 and a WAN Participant configured as server
-    // (possibly also as client)
-    RawConfiguration server_router_configuration =
-            load_configuration_from_file(server_config_path);
-
-    // Load configuration containing a Simple Participant in domain 1 and a WAN Participant configured as client
-    // (possibly also as server)
-    RawConfiguration client_router_configuration =
-            load_configuration_from_file(client_config_path);
-
     // Create DDSRouter entity whose WAN Participant is configured as server
-    DDSRouter server_router(server_router_configuration);
+    DDSRouter server_router(ddsrouter_server_configuration);
     server_router.start();
 
     // Create DDSRouter entity whose WAN Participant is configured as client
-    DDSRouter client_router(client_router_configuration);
+    DDSRouter client_router(ddsrouter_client_configuration);
     client_router.start();
 
     // Start publishing
-    while (samples_received.load() < SAMPLES_TO_RECEIVE)
+    while (samples_received.load() < samples_to_receive)
     {
         msg.index(++samples_sent);
         publisher.publish(msg);
-        std::this_thread::sleep_for(std::chrono::milliseconds(MILLISECONDS_PUBLISH_LOOP));
+
+        // If time is 0 do not wait
+        if (time_between_samples > 0)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(time_between_samples));
+        }
     }
 
     client_router.stop();
@@ -97,27 +255,103 @@ void test_WAN_communication(
  * CASES:
  *  server <-> client
  *  server <-> server-client
- *  server-client <-> server-client
+ *  server-client <-> server-client (only when basic_only is deactivate)
  */
 void test_WAN_communication_all(
-        std::string dir_path,
-        bool basic_only = false)
+        types::TransportProtocol transport_protocol,
+        types::IpVersion ip_version,
+        bool basic_only = false,
+        bool tls = false)
 {
-    // server <-> client
-    test_WAN_communication(dir_path + "server.yaml", dir_path + "client.yaml");
+    // Test architecture server <-> client
+    test::test_WAN_communication(
+        test::router_configuration(
+            test::wan_participant_configuration(
+                true, // is server 1
+                SERVER,
+                transport_protocol, // transport protocol
+                ip_version, // ip version
+                tls // tls
+                ),
+            0 // domain
+            ),
 
-    // server <-> server-client
-    test_WAN_communication(dir_path + "server.yaml", dir_path + "server-client-A.yaml");
+        test::router_configuration(
+            test::wan_participant_configuration(
+                false, // is server 1
+                CLIENT,
+                transport_protocol, // transport protocol
+                ip_version, // ip version
+                tls // tls
+                ),
+            1 // domain
+            )
+        );
+
+    // Test architecture server <-> server-client
+    test::test_WAN_communication(
+        test::router_configuration(
+            test::wan_participant_configuration(
+                true, // is server 1
+                SERVER,
+                transport_protocol, // transport protocol
+                ip_version, // ip version
+                tls // tls
+                ),
+            0 // domain
+            ),
+
+        test::router_configuration(
+            test::wan_participant_configuration(
+                false, // is server 1
+                SERVER_AND_CLIENT,
+                transport_protocol, // transport protocol
+                ip_version, // ip version
+                tls // tls
+                ),
+            1 // domain
+            )
+        );
 
     // This test is disabled for TCPv6 and TLSv6, as an underlying middleware issue resulting in no matching for this
     // scenario exists.
     // TODO: Test this as well for TCPv6 and TLSv6 cases when issue is fixed
     if (!basic_only)
     {
-        // server-client <-> server-client
-        test_WAN_communication(dir_path + "server-client-B.yaml", dir_path + "server-client-A.yaml");
+        // Test architecture server-client <-> server-client
+        test::test_WAN_communication(
+            test::router_configuration(
+                test::wan_participant_configuration(
+                    true, // is server 1
+                    SERVER_AND_CLIENT,
+                    transport_protocol, // transport protocol
+                    ip_version, // ip version
+                    tls // tls
+                    ),
+                0 // domain
+                ),
+
+            test::router_configuration(
+                test::wan_participant_configuration(
+                    false, // is server 1
+                    SERVER_AND_CLIENT,
+                    transport_protocol, // transport protocol
+                    ip_version, // ip version
+                    tls // tls
+                    ),
+                1 // domain
+                )
+            );
     }
 }
+
+} /* namespace test */
+} /* namespace core */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+using namespace eprosima::ddsrouter::core;
+using namespace eprosima::ddsrouter::core::types;
 
 /**
  * Test communication in HelloWorld topic between two DDS participants created in different domains,
@@ -126,7 +360,9 @@ void test_WAN_communication_all(
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_UDPv4)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/UDP/IPv4/");
+    test::test_WAN_communication_all(
+        types::TransportProtocol::UDP,
+        types::IpVersion::IPv4);
 }
 
 /**
@@ -136,7 +372,9 @@ TEST(DDSTestWAN, end_to_end_WAN_communication_UDPv4)
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_UDPv6)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/UDP/IPv6/");
+    test::test_WAN_communication_all(
+        types::TransportProtocol::UDP,
+        types::IpVersion::IPv6);
 }
 
 /**
@@ -146,7 +384,9 @@ TEST(DDSTestWAN, end_to_end_WAN_communication_UDPv6)
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_TCPv4)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/TCP/IPv4/");
+    test::test_WAN_communication_all(
+        types::TransportProtocol::TCP,
+        types::IpVersion::IPv4);
 }
 
 /**
@@ -156,7 +396,10 @@ TEST(DDSTestWAN, end_to_end_WAN_communication_TCPv4)
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_TCPv6)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/TCP/IPv6/", true);
+    test::test_WAN_communication_all(
+        types::TransportProtocol::TCP,
+        types::IpVersion::IPv6,
+        true);
 }
 
 /**
@@ -166,7 +409,11 @@ TEST(DDSTestWAN, end_to_end_WAN_communication_TCPv6)
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_TLSv4)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/TLS/IPv4/", false);
+    test::test_WAN_communication_all(
+        types::TransportProtocol::TCP,
+        types::IpVersion::IPv4,
+        false,
+        true);
 }
 
 /**
@@ -176,7 +423,49 @@ TEST(DDSTestWAN, end_to_end_WAN_communication_TLSv4)
  */
 TEST(DDSTestWAN, end_to_end_WAN_communication_TLSv6)
 {
-    test_WAN_communication_all("../../resources/configurations/dds/WAN/TLS/IPv6/", true);
+    test::test_WAN_communication_all(
+        types::TransportProtocol::TCP,
+        types::IpVersion::IPv6,
+        true,
+        true);
+}
+
+/**
+ * Test high throughput communication in HelloWorld topic between two DDS participants created in different domains,
+ * by using two routers with two Simple Participants at each domain, and two WAN Participants connected
+ * through UDPv4.
+ *
+ * PARAMETERS:
+ * - Frequency: 1ms
+ * - Sample size: 50K
+ * -> Throughput: 50MBps
+ */
+TEST(DDSTestWAN, end_to_end_WAN_communication_high_throughput)
+{
+    test::test_WAN_communication(
+        test::router_configuration(
+            test::wan_participant_configuration(
+                true, // is server 1
+                test::SERVER,
+                types::TransportProtocol::UDP, // transport protocol
+                types::IpVersion::IPv4 // ip version
+                ),
+            0 // domain
+            ),
+
+        test::router_configuration(
+            test::wan_participant_configuration(
+                false, // is server 1
+                test::CLIENT,
+                types::TransportProtocol::UDP, // transport protocol
+                types::IpVersion::IPv4 // ip version
+                ),
+            1 // domain
+            ),
+
+        500,
+        1,
+        1000); // 50K message size
 }
 
 int main(
