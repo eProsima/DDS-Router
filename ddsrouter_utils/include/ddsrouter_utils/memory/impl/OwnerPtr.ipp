fb23632b0646a6ebd96d92edd12d8bbee65b0e35
jparisu <69341543+jparisu@users.noreply.github.com>
2022-04-20 16:07:45 +0200

    OwnerPtr implementation (#187)
    * Refs #14277: Implementation of OwnerPtr
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add OwnerPtr tests
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add LesseePtr test
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add reset method to owner
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add bool and equal operators
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add exception when nullptr
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add Lessee default constructor and methods
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Add docstring
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: Apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14277: uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ ddsrouter_utils/include/ddsrouter_utils/memory/impl/OwnerPtr.ipp
@@ -0,0 +1,237 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file OwnerPtr.ipp
+ */
+
+#ifndef _DDSROUTERUTILS_IMPL_MEMORY_OWNERPTR_IPP_
+#define _DDSROUTERUTILS_IMPL_MEMORY_OWNERPTR_IPP_
+
+#include <ddsrouter_utils/exception/InitializationException.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace utils {
+
+template<typename T>
+LesseePtr<T>::LesseePtr()
+    : shared_mutex_(new std::mutex())
+{
+}
+
+template<typename T>
+LesseePtr<T>::LesseePtr(
+        std::weak_ptr<T> data,
+        std::shared_ptr<std::mutex> shared_mutex)
+    : data_reference_(data)
+    , shared_mutex_(shared_mutex)
+{
+}
+
+template<typename T>
+LesseePtr<T>::~LesseePtr()
+{
+    // It waits in case there are still some locked references
+    std::lock_guard<std::mutex> lock(*shared_mutex_);
+}
+
+template<typename T>
+LesseePtr<T>::LesseePtr(
+        LesseePtr<T>&& other)
+{
+    this->data_reference_ = std::move(other.data_reference_);
+    this->shared_mutex_ = std::move(other.shared_mutex_);
+}
+
+template<typename T>
+LesseePtr<T>& LesseePtr<T>::operator =(
+        LesseePtr<T>&& other)
+{
+    this->data_reference_ = std::move(other.data_reference_);
+    this->shared_mutex_ = std::move(other.shared_mutex_);
+
+    return *this;
+}
+
+template<typename T>
+std::shared_ptr<T> LesseePtr<T>::lock() noexcept
+{
+    return lock_(false);
+}
+
+template<typename T>
+std::shared_ptr<T> LesseePtr<T>::lock_with_exception()
+{
+    return lock_(true);
+}
+
+template<typename T>
+std::shared_ptr<T> LesseePtr<T>::lock_(
+        bool throw_exception)
+{
+    shared_mutex_->lock();
+
+    std::shared_ptr<T> locked_ptr = data_reference_.lock();
+
+    if (!locked_ptr)
+    {
+        this->shared_mutex_->unlock();
+
+        if (throw_exception)
+        {
+            throw InitializationException(
+                      "Trying to access a data not available anymore.");
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    // Create a different shared_ptr that points to the same element
+    return std::shared_ptr<T> (
+        locked_ptr.get(),
+        [this](T* ptr)
+        {
+            this->shared_mutex_->unlock();
+        });
+}
+
+template<typename T>
+const std::function<void(T*)> OwnerPtr<T>::DEFAULT_DELETER_ = [](T* value)
+        {
+            delete value;
+        };
+
+template<typename T>
+OwnerPtr<T>::OwnerPtr()
+{
+}
+
+template<typename T>
+OwnerPtr<T>::OwnerPtr(
+        T* reference,
+        std::function<void(T*)> deleter /* = default_deleter() */)
+{
+    if (nullptr == reference)
+    {
+        throw InitializationException(
+                  "Trying to create an OwnerPtr without a nullptr.");
+    }
+    else
+    {
+        data_reference_ = std::shared_ptr<T>(reference, deleter);
+    }
+}
+
+template<typename T>
+OwnerPtr<T>::~OwnerPtr()
+{
+    reset();
+}
+
+template<typename T>
+LesseePtr<T> OwnerPtr<T>::lease()
+{
+    std::shared_ptr<std::mutex> new_mutex = std::make_shared<std::mutex>();
+
+    leases_mutexes_.push_back(new_mutex);
+
+    return LesseePtr<T>(
+        data_reference_,
+        new_mutex);
+}
+
+template<typename T>
+void OwnerPtr<T>::reset()
+{
+    for (std::shared_ptr<std::mutex> mutex : leases_mutexes_)
+    {
+        mutex->lock();
+    }
+
+    data_reference_.reset();
+
+    for (std::shared_ptr<std::mutex> mutex : leases_mutexes_)
+    {
+        mutex->unlock();
+    }
+
+    leases_mutexes_.clear();
+}
+
+template<typename T>
+void OwnerPtr<T>::reset(
+        T* reference,
+        std::function<void(T*)> deleter /* = default_deleter() */)
+{
+    reset();
+
+    if (nullptr == reference)
+    {
+        throw InitializationException(
+                  "Trying to reset an OwnerPtr with a nullptr.");
+    }
+    else
+    {
+        data_reference_ = std::shared_ptr<T>(reference, deleter);
+    }
+}
+
+template<typename T>
+T* OwnerPtr<T>::operator ->()
+{
+    return data_reference_.operator ->();
+}
+
+template<typename T>
+T& OwnerPtr<T>::operator *()
+{
+    return data_reference_.operator *();
+}
+
+template<typename T>
+OwnerPtr<T>::operator bool() const noexcept
+{
+    return data_reference_.operator bool();
+}
+
+template<typename T>
+std::function<void(T*)> OwnerPtr<T>::default_deleter()
+{
+    return OwnerPtr<T>::DEFAULT_DELETER_;
+}
+
+template<class T>
+bool operator ==(
+        const OwnerPtr<T>& lhs,
+        std::nullptr_t) noexcept
+{
+    return !lhs;
+}
+
+template<class T>
+bool operator ==(
+        std::nullptr_t,
+        const OwnerPtr<T>& lhs) noexcept
+{
+    return !lhs;
+}
+
+} /* namespace utils */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+#endif /* _DDSROUTERUTILS_IMPL_MEMORY_OWNERPTR_IPP_ */
---
+++ ddsrouter_utils/test/unittest/memory/LesseePtrTest.cpp
@@ -0,0 +1,186 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <condition_variable>
+#include <cstring>
+#include <string>
+#include <thread>
+
+#include <gtest_aux.hpp>
+#include <gtest/gtest.h>
+
+#include <ddsrouter_utils/memory/OwnerPtr.hpp>
+
+using namespace eprosima::ddsrouter::utils;
+
+/**
+ * Create an owner and a lease from it.
+ * Check once the owner has reset the value, the lease cannot lock the data (the original value has been destroyed)
+ */
+TEST(LesseePtrTest, lessee_ptr_string_reset)
+{
+    const char* internal_str_1 = "StringTest1";
+    const char* internal_str_2 = "StringTest2";
+
+    // Create Owner
+    OwnerPtr<std::string> owner(new std::string(internal_str_1));
+
+    // Create Lessee 1
+    LesseePtr<std::string> lessee = owner.lease();
+    ASSERT_EQ(0, std::strcmp(lessee.lock()->c_str(), internal_str_1));
+
+    // Destroy element
+    owner.reset(new std::string(internal_str_2));
+
+    // Try to access lessee by lock and by operator->
+    ASSERT_FALSE(lessee.lock());
+}
+
+/**
+ * Access a value inside a \c LesseePtr class
+ */
+TEST(LesseePtrTest, lessee_ptr_string_access)
+{
+    const char* internal_str = "StringTest";
+    OwnerPtr<std::string> owner(new std::string(internal_str));
+
+    // Create Lessee
+    LesseePtr<std::string> lessee = owner.lease();
+
+    // Access by lock
+    {
+        std::shared_ptr<std::string> str_ptr = lessee.lock();
+        ASSERT_EQ(0, std::strcmp(str_ptr->c_str(), internal_str));
+    }
+}
+
+/**
+ * Access to a value inside a \c LesseePtr class from various points
+ */
+TEST(LesseePtrTest, lessee_ptr_string_multiple_access)
+{
+    const char* internal_str = "StringTest";
+    OwnerPtr<std::string> owner(new std::string(internal_str));
+
+    // Create Lessee 1
+    LesseePtr<std::string> lessee_1 = owner.lease();
+    ASSERT_EQ(0, std::strcmp(lessee_1.lock()->c_str(), internal_str));
+
+    // Create Lessee 2
+    LesseePtr<std::string> lessee_2 = owner.lease();
+    ASSERT_EQ(0, std::strcmp(lessee_2.lock()->c_str(), internal_str));
+    ASSERT_EQ(0, std::strcmp(lessee_2.lock()->c_str(), lessee_1.lock()->c_str()));
+
+    // Accessing both concurrently
+    {
+        auto ptr_1 = lessee_1.lock();
+        auto ptr_2 = lessee_2.lock();
+
+        ASSERT_EQ(0, std::strcmp(ptr_1->c_str(), ptr_2->c_str()));
+    }
+}
+
+/**
+ * Create a lease, and acces its value after destroying the value in OwnerPtr
+ */
+TEST(LesseePtrTest, lessee_ptr_string_access_after_destroy)
+{
+    const char* internal_str = "StringTest";
+
+    // Create Owner
+    OwnerPtr<std::string> owner(new std::string(internal_str));
+
+    // Create Lessee 1
+    LesseePtr<std::string> lessee = owner.lease();
+    ASSERT_EQ(0, std::strcmp(lessee.lock()->c_str(), internal_str));
+
+    // Destroy element
+    owner.reset();
+
+    // Try to access lessee by lock and by operator->
+    ASSERT_FALSE(lessee.lock());
+}
+
+/**
+ * Check that while a lessee is blocked, the owner cannot be destroyed, and must wait for lessee to destroy element
+ *
+ * This is the thread schema. A condition variable and a int will work for thread synchronization:
+ *
+ *                                  1
+ * ---TEST_THREAD---------------------try_to_destroy-----------------try_create_lease
+ *                  ---create_lease---sleep------------release_lease---
+ */
+TEST(LesseePtrTest, lessee_ptr_string_access_lock_before_destroy)
+{
+    // Create a lease and access it by lock, then trying to remove the owner should wait until the lock is released
+    const char* internal_str = "StringTest";
+
+    std::mutex cv_mutex;
+    std::condition_variable cv;
+    int synchronization_step = 0;
+
+    // Create Owner
+    OwnerPtr<std::string> owner(new std::string(internal_str));
+
+    // Create Lessee 1
+    LesseePtr<std::string> lessee = owner.lease();
+
+    // Execute thread with a reference locked
+    std::thread lease_thread ([&lessee, &cv_mutex, &cv, &synchronization_step]()
+            {
+                // Create blocking reference
+                auto reference_blocking_owner = lessee.lock();
+
+                // Set step 1 is ready
+                {
+                    std::unique_lock<std::mutex> lock(cv_mutex);
+                    synchronization_step = 1;
+                    cv.notify_all();
+                }
+
+                // Sleep for a small time to let the owner arrive to delete
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+
+                // At this point, the owner is waiting to be destroyed, but cant because this is locking it:
+                reference_blocking_owner->append("_crazy_access_to_previous_deletion_memory"); // This would break if error
+
+                // At this point the lease will be relased and the owner could be destroyed
+            });
+
+    // WAIT for step 1
+    {
+        std::unique_lock<std::mutex> lock(cv_mutex);
+        cv.wait(lock, [&]()
+                {
+                    return synchronization_step == 1;
+                });
+    }
+
+    // Destroy owner (this must wait until the lease is released)
+    owner.reset();
+
+    // Wait the thread is finished
+    lease_thread.join();
+
+    // Try to access again lessee by lock and should not have access
+    ASSERT_FALSE(lessee.lock());
+}
+
+int main(
+        int argc,
+        char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
