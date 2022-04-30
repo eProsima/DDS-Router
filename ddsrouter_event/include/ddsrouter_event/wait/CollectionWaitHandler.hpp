08f481a23fb91a93a6f20a7151c29b41769631f7
jparisu <javierparis@eprosima.com>
2022-04-30 20:45:12 +0200

    Add headers
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ ddsrouter_event/include/ddsrouter_event/wait/CollectionWaitHandler.hpp
@@ -0,0 +1,63 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file CollectionWaitHandler.hpp
+ */
+
+#ifndef _DDSROUTEREVENT_WAIT_COLLECTIONWAITHANDLER_HPP_
+#define _DDSROUTEREVENT_WAIT_COLLECTIONWAITHANDLER_HPP_
+
+#include <ddsrouter_event/wait/CounterWaitHandler.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace event {
+
+/**
+ *TODO
+ */
+template <typename T>
+class CollectionWaitHandler : protected CounterWaitHandler
+{
+public:
+
+    // Make this methods public
+    using CounterWaitHandler::enable;
+    using CounterWaitHandler::disable;
+    using CounterWaitHandler::enabled;
+    using CounterWaitHandler::stop_and_continue;
+
+    /////
+    // Add values methods
+
+    virtual void add_value(T&& value) = 0;
+
+    /////
+    // Get values methods
+
+    virtual T get_next_value() = 0;
+
+protected:
+
+    virtual T get_next_value_() = 0;
+
+    virtual T add_value_(T&& value) = 0;
+};
+
+} /* namespace event */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+#endif /* _DDSROUTEREVENT_WAIT_COLLECTIONWAITHANDLER_HPP_ */
