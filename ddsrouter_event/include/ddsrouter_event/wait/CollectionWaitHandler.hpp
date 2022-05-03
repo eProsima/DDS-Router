2b0c8ca06550e4bd38b311bf07b803dd93c19b27
jparisu <javierparis@eprosima.com>
2022-05-03 16:08:29 +0200

    Reimplement correctly DBQueueWaiter with comments and tests
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- ddsrouter_event/include/ddsrouter_event/wait/CollectionWaitHandler.hpp
+++ ddsrouter_event/include/ddsrouter_event/wait/CollectionWaitHandler.hpp
@@ -26,14 +26,26 @@ namespace ddsrouter {
 namespace event {
 
 /**
- *TODO
+ * \c CollectionWaitHandler is a class to implement a Wait Handler that store data in a collection and allow
+ * to threads to wait until some data is inserted, and remove it from the collection.
+ * The typicall use will be to implement this class with an internal queue or stack to store sorted data and
+ * consume this data from threads that may wait for a data to be available.
+ *
+ * \c T specializes this class depending on the data that is stored inside the collection.
+ *
+ * This class uses an internal CounterWaitHandler to count the number of data stored in the collection.
+ *
+ * WARNING: This class does not protect the internal collection. Making it thread safe is users duty.
+ * WARNING: The collection must be inside this handler object and should not be accessed outside this class methods.
  */
 template <typename T>
 class CollectionWaitHandler : protected CounterWaitHandler
 {
 public:
 
-    // Make this methods public
+    CollectionWaitHandler(bool enabled = true);
+
+    // Make this parent methods public
     using CounterWaitHandler::enable;
     using CounterWaitHandler::disable;
     using CounterWaitHandler::enabled;
@@ -42,19 +54,75 @@ public:
     /////
     // Add values methods
 
+    /**
+     * @brief Add a new value to the collection. Use move constructor.
+     *
+     * This method will awake ONE thread waiting for data to be available if there is any waiting
+     * Otherwise it will store the data inside the collection.
+     *
+     * @note this method calls \c add_value_ , method that must be overriden by the child class.
+     *
+     * @param value new data available
+     */
     void add_value(T&& value);
+
+    //! Add a new value to the collection. Use copy constructor.
+    void add_value(const T& value);
 
     /////
     // Get values methods
 
-    T get_next_value(
+    /**
+     * @brief Wait until there is data available in the internal collection and retrieve the first one.
+     *
+     * This method will wait until there is data available in the internal collection.
+     * In case there are data (or some data is stored while waiting) the internal collection is the one that decide
+     * which data is returned (if FIFO, LIFO, etc. depending on the collection type).
+     *
+     * @note this method calls \c get_next_value_ , method that must be overriden by the child class.
+     *
+     * @param timeout maximum time to wait for data in milliseconds. If 0, not time limit. [default 0].
+     * @return T next value available in the collection.
+     *
+     * @throw \c DisabledException if the handler is disabled when calling this method or while waiting.
+     * @throw \c TimeoutException if timeout is reached.
+     */
+    T wait_next_value(
         const utils::Duration_ms& timeout = 0);
 
 protected:
 
-    virtual T get_next_value_() = 0;
+    /**
+     * @brief Method that add a new value in the collection. Use move constructor.
+     *
+     * This method must be reimplemented in child classes specialized to the internal collection.
+     *
+     * This method is called without any mutex taken and before increase by 1 the internal counter.
+     *
+     * @param value new value
+     */
+    virtual void add_value_(T&& value) = 0;
 
-    virtual T add_value_(T&& value) = 0;
+    //! Method that add a new value in the collection. Use copy constructor.
+    virtual void add_value_(const T& value) = 0;
+
+    /**
+     * @brief Method that get next available value from the collection
+     *
+     * This method must be reimplemented in child classes specialized to the internal collection.
+     * Typically it will remove this value from the collection, but it is not mandatory.
+     *
+     * @warning This method must assure to retrieve a values at least when \c add_value_
+     * has been called more times than \c get_next_value_ .
+     *
+     * This method is called without any mutex taken and after decrease by 1 the internal counter.
+     *
+     * @return next value from the collection.
+     *
+     * @throw \c IncosistencyException if no data is available.
+     * This should not happen and is a bug from the child implementation.
+     */
+    virtual T get_next_value_() = 0;
 };
 
 } /* namespace event */
---
+++ ddsrouter_event/test/unittest/wait/DBQueueWaitHandlerTest.cpp
@@ -0,0 +1,268 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest_aux.hpp>
+#include <gtest/gtest.h>
+
+#include <ddsrouter_event/wait/DBQueueWaitHandler.hpp>
+#include <ddsrouter_utils/exception/DisabledException.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace event {
+namespace test {
+
+eprosima::ddsrouter::utils::Duration_ms DEFAULT_TIME_TEST = 1000u;
+eprosima::ddsrouter::utils::Duration_ms RESIDUAL_TIME_TEST = 10u;
+eprosima::ddsrouter::utils::Duration_ms LONG_TIME_TEST = 5000u;
+
+} /* namespace test */
+} /* namespace event */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+using namespace eprosima::ddsrouter::event;
+
+/**
+ * Check that pushing and popping values works as expected from the same thread
+ * Using int as object to use inside collection.
+ *
+ * CASES:
+ * - Push and pop one value
+ * - Push and pop multiple values
+ */
+TEST(DBQueueWaitHandlerTest, push_pop_one_thread_int)
+{
+    // Push and pop one value
+    {
+        DBQueueWaitHandler<int> handler;
+        handler.add_value(1);
+        EXPECT_EQ(handler.wait_next_value(), 1);
+    }
+
+    // Push and pop multiple values
+    {
+        DBQueueWaitHandler<int> handler;
+
+        handler.add_value(1);
+        handler.add_value(2);
+        handler.add_value(3);
+
+        EXPECT_EQ(handler.wait_next_value(), 1);
+        EXPECT_EQ(handler.wait_next_value(), 2);
+
+        handler.add_value(4);
+
+        EXPECT_EQ(handler.wait_next_value(), 3);
+        EXPECT_EQ(handler.wait_next_value(), 4);
+    }
+}
+
+/**
+ * Check that pushing and popping values works as expected from the same thread
+ * Using std::string as object to use inside collection.
+ *
+ * CASES:
+ * - Push and pop one value by moving it
+ *
+ * TODO:
+ * This test will not work as DBQueue do not allow moving values, it only copy them
+ */
+TEST(DBQueueWaitHandlerTest, push_pop_one_thread_string_move)
+{
+    // DBQueueWaitHandler<std::string> handler;
+
+    // std::string lvalue("test_data");
+
+    // handler.add_value(std::move(lvalue));
+
+    // // Getting first value
+    // std::string pop_value = handler.wait_next_value();
+
+    // EXPECT_EQ(lvalue, pop_value);
+    // EXPECT_EQ(&lvalue, &pop_value);
+
+    // // They should be same object, check that modifying one modifies the other
+    // pop_value[0] = 'a';
+    // EXPECT_EQ(lvalue, pop_value);
+}
+
+/**
+ * Check that pushing and popping values works as expected from the same thread
+ * Using std::string as object to use inside collection.
+ *
+ * CASES:
+ * - Push and pop one value by reference
+ */
+TEST(DBQueueWaitHandlerTest, push_pop_one_thread_string_copy)
+{
+    DBQueueWaitHandler<std::string> handler;
+
+    std::string lvalue("test_data");
+
+    handler.add_value(lvalue);
+
+    // Getting first value
+    std::string pop_value = handler.wait_next_value();
+    EXPECT_EQ(lvalue, pop_value);
+
+    // They should be same object, check that modifying one modifies the other
+    pop_value[0] = 'a';
+    EXPECT_NE(lvalue, pop_value);
+}
+
+/**
+ * STEPS:
+ * - 1
+ * - Create 3 threads. A & B will read wait handler, and C will write
+ * - Add 2 values [1, 2] to the handler (from C)
+ * - Get 1 value from A and 1 from B
+ * - 2
+ * - Create 3 threads. A & B will read wait handler, and C will write
+ * - Add 3 values [3, 4, 5] to the handler (from C)
+ * - Get 1 value from A and 1 from B
+ * - Get 1 value from A and 1 from B
+ * - Disable handler
+ * - Check A or B has stopped due to exception
+ */
+TEST(DBQueueWaitHandlerTest, push_one_thread_pop_many_int)
+{
+    // 1
+    {
+        bool popped_1 = false;
+        bool popped_2 = false;
+        DBQueueWaitHandler<int> handler;
+
+        // Create 3 threads. A & B will read wait handler, and C will write
+        std::thread thread_A([&]() {
+            int pop_value = handler.wait_next_value();
+            EXPECT_TRUE((pop_value == 1) || (pop_value == 2));
+            if (pop_value == 1)
+            {
+                popped_1 = true;
+            }
+            else
+            {
+                popped_2 = true;
+            }
+        });
+        std::thread thread_B([&]() {
+            int pop_value = handler.wait_next_value();
+            EXPECT_TRUE((pop_value == 1) || (pop_value == 2));
+            if (pop_value == 1)
+            {
+                popped_1 = true;
+            }
+            else
+            {
+                popped_2 = true;
+            }
+        });
+        std::thread thread_C([&]() {
+
+            handler.add_value(1);
+            handler.add_value(2);
+        });
+
+        thread_A.join();
+        thread_B.join();
+        thread_C.join();
+
+        ASSERT_TRUE(popped_1 && popped_2);
+    }
+
+    {
+        int popped_1 = 0;
+        int popped_2 = 0;
+        int popped_3 = 0;
+        int stopped_by_exception = 0;
+        DBQueueWaitHandler<int> handler;
+
+        auto lambda_for_A_and_B =
+            [&popped_1, &popped_2, &popped_3, &stopped_by_exception, &handler]
+            ()
+            {
+                try
+                {
+                    // Wait for first value from queue
+                    int pop_value = handler.wait_next_value();
+                    if (pop_value == 1)
+                    {
+                        popped_1++;
+                    }
+                    else if (pop_value == 2)
+                    {
+                        popped_2++;
+                    }
+                    else if (pop_value == 3)
+                    {
+                        popped_3++;
+                    }
+                    // Wait for second value from queue
+                    pop_value = handler.wait_next_value();
+                    if (pop_value == 1)
+                    {
+                        popped_1++;
+                    }
+                    else if (pop_value == 2)
+                    {
+                        popped_2++;
+                    }
+                    else if (pop_value == 3)
+                    {
+                        popped_3++;
+                    }
+                }
+                catch (const eprosima::ddsrouter::utils::DisabledException & e)
+                {
+                    // Stopped by disabled
+                    stopped_by_exception++;
+                }
+
+            };
+
+        // Create 3 threads. A & B will read wait handler, and C will write
+        std::thread thread_A(lambda_for_A_and_B);
+        std::thread thread_B(lambda_for_A_and_B);
+
+        std::thread thread_C([&]() {
+            handler.add_value(1);
+            handler.add_value(2);
+            handler.add_value(3);
+        });
+
+        thread_C.join();
+
+        // Wait for notification to arrive to waiting threads
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+        handler.disable();
+
+        thread_A.join();
+        thread_B.join();
+
+        ASSERT_TRUE(1 == popped_1);
+        ASSERT_TRUE(1 == popped_2);
+        ASSERT_TRUE(1 == popped_3);
+        ASSERT_TRUE(1 == stopped_by_exception);
+    }
+}
+
+int main(
+        int argc,
+        char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
