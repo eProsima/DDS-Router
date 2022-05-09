4783142f56c60f70ed5fc02d20675feab4369cd5
jparisu <69341543+jparisu@users.noreply.github.com>
2022-05-09 08:49:50 +0200

    Implement Counter Wait Handler (#203)
    * Implement Counter Wait Handler
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Add comparison equal to threshold methods
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ ddsrouter_event/include/ddsrouter_event/wait/CounterWaitHandler.hpp
@@ -0,0 +1,130 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file CounterWaitHandler.hpp
+ */
+
+#ifndef _DDSROUTEREVENT_WAIT_COUNTERWAITHANDLER_HPP_
+#define _DDSROUTEREVENT_WAIT_COUNTERWAITHANDLER_HPP_
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+
+#include <ddsrouter_event/wait/WaitHandler.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace event {
+
+using CounterType = int32_t;
+
+/**
+ * @brief This WaitHandler allows to wait for an int value
+ *
+ * This works as an int value that allows to wait for a specific value or threshold.
+ */
+class CounterWaitHandler : protected WaitHandler<CounterType>
+{
+public:
+
+    /**
+     * @brief Construct a new Counter Wait Handler object
+     *
+     * @param value to initialize counter
+     * @param enabled whether the object starts enabled or disabled
+     */
+    CounterWaitHandler(
+            CounterType value,
+            bool enabled = true);
+
+    //! Default constructor
+    ~CounterWaitHandler();
+
+    /////
+    // Enabling methods
+
+    // Make this methods public
+    using WaitHandler<CounterType>::enable;
+    using WaitHandler<CounterType>::disable;
+    using WaitHandler<CounterType>::enabled;
+    using WaitHandler<CounterType>::set_value;
+    using WaitHandler<CounterType>::get_value;
+    using WaitHandler<CounterType>::stop_and_continue;
+
+    /////
+    // Wait methods
+
+    /**
+     * @brief Wait current thread while counter is not \c expected_value .
+     *
+     * @param expected_value value of counter for which the thread will awake
+     * @param timeout maximum time in milliseconds that should wait until awaking for timeout
+     *
+     * @return reason why thread was awake
+     */
+    AwakeReason wait_equal(
+            CounterType expected_value,
+            const utils::Duration_ms& timeout = 0);
+
+    /**
+     * @brief Wait current thread while counter is lower or equal than \c upper_bound .
+     *
+     * @param upper_bound maximum value of counter with which the thread will not awake
+     * @param timeout maximum time in milliseconds that should wait until awaking for timeout
+     *
+     * @return reason why thread was awake
+     */
+    AwakeReason wait_greater_than(
+            CounterType upper_bound,
+            const utils::Duration_ms& timeout = 0);
+
+    //! @brief Wait current thread while counter is lower than \c upper_bound .
+    AwakeReason wait_greater_equal_than(
+            CounterType upper_bound,
+            const utils::Duration_ms& timeout = 0);
+
+    /**
+     * @brief Wait current thread while counter is higher or equal than \c lower_bound .
+     *
+     * @param lower_bound minimum value of counter with which the thread will not awake
+     * @param timeout maximum time in milliseconds that should wait until awaking for timeout
+     *
+     * @return reason why thread was awake
+     */
+    AwakeReason wait_lower_than(
+            CounterType lower_bound,
+            const utils::Duration_ms& timeout = 0);
+
+    //! Wait current thread while counter is higher than \c lower_bound .
+    AwakeReason wait_lower_equal_than(
+            CounterType lower_bound,
+            const utils::Duration_ms& timeout = 0);
+
+    /////
+    // Value methods
+
+    //! Operator prefix ++ to add 1 to counter
+    CounterWaitHandler& operator ++();
+
+    //! Operator prefix -- to substract 1 to counter
+    CounterWaitHandler& operator --();
+};
+
+} /* namespace event */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+#endif /* _DDSROUTEREVENT_WAIT_COUNTERWAITHANDLER_HPP_ */
---
+++ ddsrouter_event/test/unittest/wait/CounterWaitHandlerTest.cpp
@@ -0,0 +1,332 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <memory>
+#include <thread>
+
+#include <gtest_aux.hpp>
+#include <gtest/gtest.h>
+
+#include <TestLogHandler.hpp>
+
+#include <ddsrouter_event/wait/CounterWaitHandler.hpp>
+
+namespace eprosima {
+namespace ddsrouter {
+namespace event {
+namespace test {
+
+eprosima::ddsrouter::utils::Duration_ms DEFAULT_TIME_TEST = 1000u;
+eprosima::ddsrouter::utils::Duration_ms RESIDUAL_TIME_TEST = 10u;
+eprosima::ddsrouter::utils::Duration_ms LONG_TIME_TEST = 5000u;
+
+} /* namespace test */
+} /* namespace event */
+} /* namespace ddsrouter */
+} /* namespace eprosima */
+
+using namespace eprosima::ddsrouter::event;
+
+/**
+ * Create a \c CounterWaitHandler and check its internal values with different numbers
+ *
+ * Change the internal values and check if the values are changed
+ */
+TEST(CounterWaitHandlerTest, value_handler)
+{
+    std::vector<CounterType> counter_values = {0, 1, 12345, -1, -99999};
+
+    for (CounterType i : counter_values)
+    {
+        CounterWaitHandler waiter(i);
+
+        ASSERT_EQ(waiter.get_value(), i);
+
+        // Check operator++
+        ++waiter;
+        ASSERT_EQ(waiter.get_value(), i + 1);
+
+        // Check operator--
+        --waiter;
+        ASSERT_EQ(waiter.get_value(), i);
+
+        // Check set value
+        waiter.set_value(2);
+        ASSERT_EQ(waiter.get_value(), 2);
+    }
+}
+
+/**
+ * Test wait for a specific value.
+ *
+ * Wait for 1 and set value to 1.
+ * Wait for 2 and set value to 3, and close it by disabling
+ */
+TEST(CounterWaitHandlerTest, wait_value)
+{
+    // Wait for 1 and set value to 1.
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                // Note that if this wait occurs after set_value
+                // it will jut not wait but awake reason will be correct.
+                AwakeReason reason = waiter.wait_equal(1);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(1);
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for 2 and set value to 3, and close it by disabling
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_equal(2);
+                ASSERT_EQ(reason, AwakeReason::DISABLED);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(3);
+
+        // Disable waiter as it will not awake by value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.disable();
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+}
+
+/**
+ * Test wait for a specific value.
+ *
+ * Wait for 3 and set value to 5.
+ * Wait for -1 and let it awake alone as initial is 0.
+ * Wait for ge 13 and set value to 13
+ * Wait for 17 and set value to 13, and close it by disabling
+ */
+TEST(CounterWaitHandlerTest, wait_upper)
+{
+    // Wait for 3 and set value to 5.
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                // Note that if this wait occurs after set_value
+                // it will jut not wait but awake reason will be correct.
+                AwakeReason reason = waiter.wait_greater_than(3);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(5);
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for -1 and let it awake alone as initial is 0.
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_greater_than(-1);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for ge 13 and set value to 13
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_greater_equal_than(13);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+        waiter.set_value(13);
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    //  Wait for 17 and set value to 13, and close it by disabling
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_greater_than(17); // Wait for disable
+                ASSERT_EQ(reason, AwakeReason::DISABLED);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(13);
+
+        // Disable waiter as it will not awake by value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.disable();
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+}
+
+/**
+ * Test wait for a specific value.
+ *
+ * Wait for -3 and set value to -5.
+ * Wait for 1 and let it awake alone as initial is 0.
+ * Wait for le 13 and set value to 13
+ * Wait for -17 and set value to -13, and close it by disabling
+ */
+TEST(CounterWaitHandlerTest, wait_lower)
+{
+    // Wait for 3 and set value to 5.
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                // Note that if this wait occurs after set_value
+                // it will jut not wait but awake reason will be correct.
+                AwakeReason reason = waiter.wait_lower_than(-3);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(-5);
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for -1 and let it awake alone as initial is 0.
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_lower_than(1);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for le 13 and set value to 13
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_lower_equal_than(13);
+                ASSERT_EQ(reason, AwakeReason::CONDITION_MET);
+            }
+            );
+        waiter.set_value(13);
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+
+    // Wait for 2 and set value to 3, and close it by disabling
+    {
+        CounterWaitHandler waiter(0); // It starts closed and enabled
+
+        // Create a thread that waits for the waiter to open
+        std::thread waiting_thread(
+            [&waiter]()
+            {
+                // Wait in new thread
+                AwakeReason reason = waiter.wait_lower_than(-17); // Wait for disable
+                ASSERT_EQ(reason, AwakeReason::DISABLED);
+            }
+            );
+
+        // Wait a bit for waiter to change value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.set_value(-13);
+
+        // Disable waiter as it will not awake by value
+        std::this_thread::sleep_for(std::chrono::milliseconds(test::RESIDUAL_TIME_TEST));
+        waiter.disable();
+
+        // Wait for the thread to finish
+        waiting_thread.join();
+    }
+}
+
+int main(
+        int argc,
+        char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
