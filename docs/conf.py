71d940df42e1130632cb381943b3c7bbf19fb2f5
RaulSanchez <raul@eprosima.com>
2022-08-08 10:32:21 +0200

    Update documentation logo
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- docs/conf.py
+++ docs/conf.py
@@ -301,8 +301,10 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -318,8 +320,8 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#
-# html_logo = None
+
+html_logo = 'rst/_static/css/imgs/ddsrouter_logo_white_stroke.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
