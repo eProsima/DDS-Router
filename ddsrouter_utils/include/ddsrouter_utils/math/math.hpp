58b11893d8ba48a30b030130346c96d5158a284a
jparisu <javierparis@eprosima.com>
2022-08-12 09:18:30 +0200

    Increment math methods
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- ddsrouter_utils/include/ddsrouter_utils/math/math.hpp
+++ ddsrouter_utils/include/ddsrouter_utils/math/math.hpp
@@ -30,23 +30,83 @@ namespace ddsrouter {
 namespace utils {
 
 /**
+ * @brief Optimize % 2 operation
+ *
+ * @param number
+ *
+ * @return whether \c number is odd
+ */
+DDSROUTER_UTILS_DllAPI bool is_even(
+        uint32_t number) noexcept;
+
+/**
  * @brief Module (%) operation with performance optimization
  *
  * This function optimizes the % operation, that executes a division, by optimizing these cases:
- * - If the dividend is smaller or equal than the divisor, the result is the dividend
+ * - If the dividend is smaller than the divisor, the result is the dividend
+ * - If the dividend is equal than the divisor, the result is 0
  * - If the divisor is 2, the result is the dividend % 2 calculated by a logic AND operation
  * - If the divisor is a power of 2, the result is calculated by a logic AND operation
+ * - Otherwise uses % operation
  *
  * @param dividend Dividend
  * @param divisor Divisor (must be greater than 0 so the operation make sense)
  *
- * @attention if divisor is 0, the result is \c dividend
+ * @pre \c divisor must not be 0
  *
- * @return The result of the operation
+ * @return The result of the operation %
+ *
+ * @attention Do only use this function with non literal values. Literal values are optimized by compiler.
  */
 DDSROUTER_UTILS_DllAPI uint32_t fast_module(
         uint32_t dividend,
         uint32_t divisor) noexcept;
+
+/**
+ * @brief Integer Division (/) operation with performance optimization
+ *
+ * This function optimizes the / operation by optimizing these cases:
+ * - If \c dividend is smaller or equal than the \c, the result is \c dividend
+ * - If the \c is 2, the result is \c dividend % 2 calculated by a logic AND operation
+ * - If the \c is a power of 2, the result is calculated by a logic AND operation
+ * - Otherwise uses / operation
+ *
+ * @param dividend Dividend
+ * @param divisor Divisor
+ *
+ * @pre \c divisor must not be 0
+ *
+ * @return The result of the operation /
+ *
+ * @attention Do only use this function with non literal values. Literal values are optimized by compiler.
+ */
+DDSROUTER_UTILS_DllAPI uint32_t fast_division(
+        uint32_t dividend,
+        uint32_t divisor) noexcept;
+
+/**
+ * @brief Calculate the sum of an arithmetic progression from an initial to a final number.
+ *
+ * This function uses the fast operation to calculate an arithmetic sum:
+ * S = ((a1 + an) / 2) * (n)
+ *
+ * @pre \c interval must be greater than 0
+ * @pre \c steps must be greater than 0
+ *
+ * @param lowest lowest element of the arithmetic progression
+ * @param interval interval between two elements of the arithmetic progression
+ * @param steps number of steps of the arithmetic progression
+ *
+ * @return The result of the sum
+ *
+ * EXAMPLE OF USE
+ *   1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = arithmetic_progression_sum(1, 1, 10)
+ *   0 + 2 + 4 + 6 + 8 = arithmetic_progression_sum(0, 2, 5)
+ */
+DDSROUTER_UTILS_DllAPI uint32_t arithmetic_progression_sum(
+        uint32_t lowest,
+        uint32_t interval,
+        uint32_t steps) noexcept;
 
 } /* namespace utils */
 } /* namespace ddsrouter */
