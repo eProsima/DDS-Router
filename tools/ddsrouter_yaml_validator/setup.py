b10bb9883369f743225ad77bb45ab676230bae31
jparisu <javierparis@eprosima.com>
2022-10-04 11:10:24 +0200

    version Bump to 1.0.0
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- tools/ddsrouter_yaml_validator/setup.py
+++ tools/ddsrouter_yaml_validator/setup.py
@@ -6,7 +6,7 @@ package_name = 'ddsrouter_yaml_validator'
 
 setup(
     name=package_name,
-    version='0.4.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
