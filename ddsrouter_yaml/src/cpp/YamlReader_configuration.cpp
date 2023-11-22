89b774505f38e312f3dfa47d444da863ee16313f
tempate <danieldiaz@eprosima.com>
2023-11-22 12:51:30 +0100

    Apply suggestions
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- ddsrouter_yaml/src/cpp/YamlReader_configuration.cpp
+++ ddsrouter_yaml/src/cpp/YamlReader_configuration.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cpp_utils/enum/EnumBuilder.hpp>
-
 #include <ddspipe_participants/configuration/DiscoveryServerParticipantConfiguration.hpp>
 #include <ddspipe_participants/configuration/EchoParticipantConfiguration.hpp>
 #include <ddspipe_participants/configuration/InitialPeersParticipantConfiguration.hpp>
