2f5d4e28bd4734c6d34d6fbc189c314e95cbe87b
tempate <danieldiaz@eprosima.com>
2024-03-05 13:02:12 +0100

    Delete the log consumers before closing
    Signed-off-by: tempate <danieldiaz@eprosima.com>

--- tools/ddsrouter_tool/src/cpp/main.cpp
+++ tools/ddsrouter_tool/src/cpp/main.cpp
@@ -279,5 +279,8 @@ int main(
     // Force print every log before closing
     eprosima::utils::Log::Flush();
 
+    // Delete the consumers before closing
+    eprosima::utils::Log::ClearConsumers();
+
     return static_cast<int>(ui::ProcessReturnCode::success);
 }
