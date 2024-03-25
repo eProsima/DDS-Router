aa247e9928b44d29a7f59589b7363d557eb08169
Irene Bandera Moreno <irenebandera@eprosima.com>
2024-03-25 16:48:58 +0100

    Bump version to v2.2.0 (#443)
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- tools/ddsrouter_yaml_validator/setup.py
+++ tools/ddsrouter_yaml_validator/setup.py
@@ -6,7 +6,7 @@ package_name = 'ddsrouter_yaml_validator'
 
 setup(
     name=package_name,
-    version='2.1.0',
+    version='2.2.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
