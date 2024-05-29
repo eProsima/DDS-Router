962a4cf0ee03623095859f7750fa980042ac3136
Lucia Echevarria <luciaechevarria@eprosima.com>
2024-05-29 08:26:38 +0200

    Update repository for Fast DDS 3.0.0 compatibility
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>

--- ddsrouter_yaml/project_settings.cmake
+++ ddsrouter_yaml/project_settings.cmake
@@ -25,14 +25,14 @@ set(MODULE_SUMMARY
 set(MODULE_FIND_PACKAGES
     yaml-cpp
     fastcdr
-    fastrtps
+    fastdds
     cpp_utils
     ddspipe_core
     ddspipe_participants
     ddspipe_yaml
     ddsrouter_core)
 
-set(fastrtps_MINIMUM_VERSION "2.8")
+set(fastdds_MINIMUM_VERSION "3.0.0")
 
 set(MODULE_DEPENDENCIES
     $<$<BOOL:${WIN32}>:iphlpapi$<SEMICOLON>Shlwapi>
